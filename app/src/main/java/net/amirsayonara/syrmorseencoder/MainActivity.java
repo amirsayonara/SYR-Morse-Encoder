9f92e041162ff1f1aff7581db9a42667d0a08bcf
Amir Sayonara <amirsayonara@gmail.com>
2018-09-12 21:15:37 +0700

    penambahan pengaturan custom pengguna

--- app/src/main/java/net/amirsayonara/syrmorseencoder/MainActivity.java
+++ app/src/main/java/net/amirsayonara/syrmorseencoder/MainActivity.java
@@ -15,6 +15,12 @@ public class MainActivity extends AppCompatActivity {
 
     public void kerjakan(View view) {
         SyrMorseEncoder morse = new SyrMorseEncoder();
+        EditText titik = (EditText) findViewById(R.id.tandatitik);
+        EditText strip = (EditText) findViewById(R.id.tandastrip);
+        EditText pemisah = (EditText) findViewById(R.id.tandapemisah);
+        morse.setTitik(titik.getText().toString());
+        morse.setStrip(strip.getText().toString());
+        morse.setPemisah(pemisah.getText().toString());
         EditText input = (EditText) findViewById(R.id.teks_input);
         EditText output = (EditText) findViewById(R.id.teks_output);
         output.setText(morse.encode(input.getText().toString()));
--- app/src/main/java/net/amirsayonara/syrmorseencoder/SyrMorseEncoder.java
+++ app/src/main/java/net/amirsayonara/syrmorseencoder/SyrMorseEncoder.java
@@ -8,17 +8,20 @@ import java.util.Set;
 public class SyrMorseEncoder {
     private Map data = new HashMap();
     private String[] morse = "10 0111 0101 011 1 1101 001 1111 11 1000 010 1011 00 01 000 1001 0010 101 111 0 110 1110 100 0110 0100 0011".split(" ");
-    private String titik=".", strip="-", pemisah="/";
+    private String titik = ".", strip = "-", pemisah = "/";
     public SyrMorseEncoder() {
+        reload();
+    }
+    private void reload() {
         for (int i=0; i<morse.length; i++) {
             data.put((char) (i + 65), morse[i].replace('1', titik.charAt(0)).replace('0', strip.charAt(0)));
         }
     }
     public void setTitik(String titik) {
-        this.titik = titik;
+        this.titik = titik;reload();
     }
     public void setStrip(String strip) {
-        this.strip = strip;
+        this.strip = strip;reload();
     }
     public void setPemisah(String pemisah) {
         this.pemisah = pemisah;
@@ -27,10 +30,10 @@ public class SyrMorseEncoder {
         String tmp = "";teks=teks.toUpperCase();
         Set alpha = data.keySet();
         for (int i=0; i<teks.length(); i++) {
-            if (alpha.contains(teks.charAt(i))) tmp += data.get(teks.charAt(i))+ "/";
-            else if (teks.charAt(i)==' ') tmp += '/';
+            if (alpha.contains(teks.charAt(i))) tmp += data.get(teks.charAt(i))+pemisah;
+            else if (teks.charAt(i)==' ') tmp += pemisah;
             else tmp += teks.charAt(i);
         }
-        return tmp+'/';
+        return tmp+pemisah;
     }
 }
