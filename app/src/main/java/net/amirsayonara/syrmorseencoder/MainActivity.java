a390c3d3c7e7575909b9bb4846b2ca0320e63d33
Amir Sayonara <amirsayonara@gmail.com>
2018-09-12 20:38:38 +0700

    penambahan fitur encode

--- app/src/main/java/net/amirsayonara/syrmorseencoder/MainActivity.java
+++ app/src/main/java/net/amirsayonara/syrmorseencoder/MainActivity.java
@@ -2,6 +2,8 @@ package net.amirsayonara.syrmorseencoder;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void kerjakan(View view) {
+        SyrMorseEncoder morse = new SyrMorseEncoder();
+        EditText input = (EditText) findViewById(R.id.teks_input);
+        EditText output = (EditText) findViewById(R.id.teks_output);
+        output.setText(morse.encode(input.getText().toString()));
     }
 }
--- app/src/main/java/net/amirsayonara/syrmorseencoder/SyrMorseEncoder.java
+++ app/src/main/java/net/amirsayonara/syrmorseencoder/SyrMorseEncoder.java
@@ -3,14 +3,15 @@ package net.amirsayonara.syrmorseencoder;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Set;
 
 public class SyrMorseEncoder {
-    private static Map data = new HashMap();
-    private static String[] morse = "10 0111 0101 011 1 1101 001 1111 11 1000 010 1011 00 01 000 1001 0010 101 111 0 110 1110 100 0110 0100 0011".split(" ");
-    private static String titik=".", strip="-", pemisah="/";
-    public static void main(String[] args) {
+    private Map data = new HashMap();
+    private String[] morse = "10 0111 0101 011 1 1101 001 1111 11 1000 010 1011 00 01 000 1001 0010 101 111 0 110 1110 100 0110 0100 0011".split(" ");
+    private String titik=".", strip="-", pemisah="/";
+    public SyrMorseEncoder() {
         for (int i=0; i<morse.length; i++) {
-            data.put((char) (i + 65), morse[i].replace('1', titik.charAt(0)).replace('0', titik.charAt(0)));
+            data.put((char) (i + 65), morse[i].replace('1', titik.charAt(0)).replace('0', strip.charAt(0)));
         }
     }
     public void setTitik(String titik) {
@@ -22,11 +23,14 @@ public class SyrMorseEncoder {
     public void setPemisah(String pemisah) {
         this.pemisah = pemisah;
     }
-    public String enkripsi(String teks) {
-        String tmp = "";teks.toUpperCase();
+    public String encode(String teks) {
+        String tmp = "";teks=teks.toUpperCase();
+        Set alpha = data.keySet();
         for (int i=0; i<teks.length(); i++) {
-            tmp += data.get(teks.charAt(i));
+            if (alpha.contains(teks.charAt(i))) tmp += data.get(teks.charAt(i))+ "/";
+            else if (teks.charAt(i)==' ') tmp += '/';
+            else tmp += teks.charAt(i);
         }
-        return tmp;
+        return tmp+'/';
     }
 }
